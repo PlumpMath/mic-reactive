e189600357e87fccfec03a2f02bf4d9fdfd5dafd
Mike Belanger <mikebelanger@fastmail.com>
2016-12-03 11:58:14 -0500

    Now audio-reactive!

--- src/mic_reactive/live.cljs
+++ src/mic_reactive/live.cljs
@@ -17,39 +17,45 @@
 (def analyser (new js/Tone.Analyser (clj->js {:type "waveform"
                                               :size 256})))
 
+;;Connect our microphone to Tonejs' analyser node
 (.connect mic analyser)
 
+;;I don't feel like finding the library for averaging.
 (defn average [input-array]
-  (/ (reduce + (js->clj input-array))
+  (/ (reduce + input-array)
      (count input-array)))
 
-(.open mic (fn []
+;;Open the mic to change audio-data
+(.open mic)
 
-  (def actions
-    {:expand-shrink (fn [obj time]
-                                 (do
-                                  (let [values (list (.analyse analyser))
-                                        max-val (max values)]
-                                    (.log js/console values))
-                                  (match
-                                   [(mod (Math.round time) 2)]
-                                    [0] (.set-scale transform obj 2.3)
-                                    [1] (.set-scale transform obj 0.75))
-                                  (.set-rotation-euler transform obj
-                                         (* time 1.0)
-                                         3
-                                         (* time 0.3))
-                                  (.set-diffuse-color material obj
-                                              "Material"
-                                              (.from-values rgba
-                                                            100
-                                                            100
-                                                            200
-                                                            1))
-                                  (.set-diffuse-intensity material obj
-                                              "Material"
-                                              (* time 0.0005))))})))
+(def actions
+  {:expand-shrink (fn [obj time]
+                (let [raw-audio (aclone (.analyse analyser))
+                      audio {:min (apply min raw-audio)
+                             :max (apply max raw-audio)
+                             :avg (average raw-audio)}]
+                 (do
+                  ;;Now initialize the mic
+                  (.start mic)
+
+                  ;;Change scale of monkey head to audio data
+                  (.set-scale transform obj
+                      (/ (:min audio) 90))
+                  (.set-rotation-euler transform obj
+                         (* time 1.0)
+                         2
+                         (* time 0.3))
+                  (.set-diffuse-color material obj
+                              "Material"
+                              (.from-values rgba
+                                (/ (:min audio) 25)
+                                (/ (:max audio) 20)
+                                (/ (:min audio) 25)
+                                1))
+                  (.set-diffuse-intensity material obj
+                              "Material"
+                              (/ (:min audio) 255)))))})
 
 (defn continuous-timeline-sensor [time]
-  (let [monkey (.get-object-by-name scenes "Suzanne")]
+  (let [monkey     (.get-object-by-name scenes "Suzanne")]
        ((:expand-shrink actions) monkey time)))
